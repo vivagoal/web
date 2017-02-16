9a93e0ea94c3d39986901d9a2ffbc7f8daa1d511
dwi <dwi@DESKTOP-VJMEC8V>
2017-02-16 15:09:44 +0700

    - update first

---
+++ themes/default/layout/main_layout.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>{TITLE_PAGE}</title>
+        <meta tag="keywords" content="{KEYWORDS}" >
+        
+    </head>
+    <body>        
+        {HEADER_SECTION}
+        {CONTENT_SECTION}
+        {FOOTER_SECTION}
+    </body>
+    
+</html>
